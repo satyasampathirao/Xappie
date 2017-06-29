207d6e9362efa47395cf9c6d253d99427b0dce55
Shankar Pilli <shankar.pilli@sparity.com>
2017-06-29 11:09:53 +0530

    Added close icon and header bg in the project

--- app/src/main/java/com/xappie/activities/AppTourActivity.java
+++ app/src/main/java/com/xappie/activities/AppTourActivity.java
@@ -4,36 +4,49 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.widget.TextView;
 
 import com.xappie.R;
 import com.xappie.adapters.AdapterforTour;
 import com.xappie.customviews.CirclePageIndicatorForTour;
+import com.xappie.utils.Utility;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AppTourActivity extends Activity {
-	public static ViewPager mViewPager;
-	private AdapterforTour mAdapterforTour;
-	private CirclePageIndicatorForTour cri;
+    public static ViewPager mViewPager;
+    private AdapterforTour mAdapterforTour;
+    private CirclePageIndicatorForTour cri;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.app_tour_activity);
-		
-		mViewPager = (ViewPager) findViewById(R.id.mViewPager);
-		cri = (CirclePageIndicatorForTour) findViewById(R.id.indicator);
-		mViewPager.setOffscreenPageLimit(5);
-		mAdapterforTour = new AdapterforTour(this);
-		mViewPager.setAdapter(mAdapterforTour);
-		cri.setViewPager(mViewPager);
-	}
+    @BindView(R.id.tv_close_icon)
+    TextView tv_close_icon;
 
-	@Override
-	public void onBackPressed() {
-		Intent startMain = new Intent(Intent.ACTION_MAIN);
-		startMain.addCategory(Intent.CATEGORY_HOME);
-		startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		startMain.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		startActivity(startMain);
-		finish();
-	}
+    @BindView(R.id.tv_xappie_tour)
+    TextView tv_xappie_tour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_tour_activity);
+        ButterKnife.bind(this);
+        mViewPager = (ViewPager) findViewById(R.id.mViewPager);
+        cri = (CirclePageIndicatorForTour) findViewById(R.id.indicator);
+        mViewPager.setOffscreenPageLimit(5);
+        mAdapterforTour = new AdapterforTour(this);
+        mViewPager.setAdapter(mAdapterforTour);
+        cri.setViewPager(mViewPager);
+        tv_close_icon.setTypeface(Utility.getMaterialIconsRegular(this));
+        tv_xappie_tour.setTypeface(Utility.geOpenSansRegular(this));
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(startMain);
+        finish();
+    }
 }
---
+++ app/src/main/java/com/xappie/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.xappie.utils;
+
+/**
+ * Created by Shankar on 6/29/2017.
+ */
+
+public class Constants {
+    // Log message On or Off
+    public static final boolean logMessageOnOrOff = true;
+
+    /* shared pref */
+    public static final String APP_PREF = "Xappie_Pref";
+}
