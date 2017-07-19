9dc039292023ef8fb43e056c5343f75fd51281d0
sixscorpions <sandy0210>
2017-07-19 08:40:31 +0530

    Added xappie place holder and home page view holder

--- app/src/main/java/com/xappie/fragments/HomeFragment.java
+++ app/src/main/java/com/xappie/fragments/HomeFragment.java
@@ -3,11 +3,17 @@ package com.xappie.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.xappie.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import com.xappie.customviews.CirclePageIndicatorForTour;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,16 +22,19 @@ public class HomeFragment extends Fragment {
 
     public static final String TAG = HomeFragment.class.getSimpleName();
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
+    @BindView(R.id.card_pager)
+    ViewPager card_pager;
+
+    @BindView(R.id.indicator)
+    CirclePageIndicatorForTour indicator;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
 
 }
