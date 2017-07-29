3b055a2c6ba43f262b72719a55d837b0e7738cc7
shankar <shankar.pilli@sparity.com>
2017-07-29 18:24:39 +0530

    Added all my events list adapter

---
+++ app/src/main/java/com/xappie/adapters/AllMyEventsListAdapter.java
@@ -0,0 +1,90 @@
+package com.xappie.adapters;
+
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.xappie.R;
+import com.xappie.activities.DashBoardActivity;
+import com.xappie.fragments.ClassifiedsDetailFragment;
+import com.xappie.models.EntertainmentModel;
+import com.xappie.utils.Utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Shankar on 28/07/2017
+ */
+
+public class AllMyEventsListAdapter extends BaseAdapter {
+
+    private DashBoardActivity mDashBoardActivity;
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<EntertainmentModel> entertainmentModels;
+    private Typeface mOpenSansBoldTypeface;
+    private Typeface mOpenSansRegularTypeface;
+
+    public AllMyEventsListAdapter(DashBoardActivity mDashBoardActivity, ArrayList<EntertainmentModel> entertainmentModels) {
+        this.mDashBoardActivity = mDashBoardActivity;
+        mLayoutInflater = LayoutInflater.from(mDashBoardActivity);
+        this.entertainmentModels = entertainmentModels;
+        mOpenSansBoldTypeface = Utility.getOpenSansBold(mDashBoardActivity);
+        mOpenSansRegularTypeface = Utility.getOpenSansRegular(mDashBoardActivity);
+    }
+
+    @Override
+    public int getCount() {
+        return entertainmentModels.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ClassifiedsListHolder mClassifiedsListHolder = null;
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.all_events_list_item,
+                    null);
+            mClassifiedsListHolder = new ClassifiedsListHolder();
+            mClassifiedsListHolder.img_logo = (ImageView) convertView.findViewById(R.id.img_logo);
+            mClassifiedsListHolder.tv_title = (TextView) convertView.findViewById(R.id.tv_title);
+            mClassifiedsListHolder.tv_time = (TextView) convertView.findViewById(R.id.tv_time);
+            mClassifiedsListHolder.tv_location = (TextView) convertView.findViewById(R.id.tv_location);
+
+            mClassifiedsListHolder.tv_title.setTypeface(mOpenSansBoldTypeface);
+            mClassifiedsListHolder.tv_time.setTypeface(mOpenSansRegularTypeface);
+            mClassifiedsListHolder.tv_location.setTypeface(mOpenSansRegularTypeface);
+
+            convertView.setTag(mClassifiedsListHolder);
+        } else {
+            mClassifiedsListHolder = (ClassifiedsListHolder) convertView.getTag();
+        }
+
+        EntertainmentModel entertainmentModel = entertainmentModels.get(position);
+        // mClassifiedsListHolder.tv_title.setText(entertainmentModel.getTitle());
+        mClassifiedsListHolder.tv_title.setText("Lorem ipsum is simply");
+        mClassifiedsListHolder.tv_time.setText(Utility.getResourcesString(mDashBoardActivity, R.string.ravi_kiran).toUpperCase());
+
+
+        return convertView;
+    }
+
+    private class ClassifiedsListHolder {
+        TextView tv_title;
+        TextView tv_time;
+        TextView tv_location;
+        ImageView img_logo;
+    }
+}
--- app/src/main/java/com/xappie/fragments/AllMyEventsFragment.java
+++ app/src/main/java/com/xappie/fragments/AllMyEventsFragment.java
@@ -1,16 +1,27 @@
 package com.xappie.fragments;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.xappie.R;
 import com.xappie.activities.DashBoardActivity;
+import com.xappie.adapters.AllEventsListAdapter;
+import com.xappie.adapters.AllMyEventsListAdapter;
+import com.xappie.models.EntertainmentModel;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,7 +39,7 @@ public class AllMyEventsFragment extends Fragment {
      * AllEvents List set up
      */
     @BindView(R.id.listView)
-    ListView listView;
+    SwipeMenuListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,11 +58,74 @@ public class AllMyEventsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initUI();
+        setGridViewData();
     }
 
-    private void initUI() {
+    /*This method is used to set the lsit view data*/
+    private void setGridViewData() {
+        AllMyEventsListAdapter allEventsListAdapter = new AllMyEventsListAdapter(mParent, getEntertainData());
+        // step 1. create a MenuCreator
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
 
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getActivity().getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xAD, 0x24,
+                        0x3E)));
+                // set item width
+                openItem.setWidth(dp2px(90));
+                // set item title
+                openItem.setTitle("Edit");
+                // set item title fontsize
+                openItem.setTitleSize(15);
+                openItem.setIcon(R.drawable.ic_mode_edit_white_24dp);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+
+                // create "open" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getActivity().getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xEF, 0x4E,
+                        0x54)));
+                // set item width
+                deleteItem.setWidth(dp2px(90));
+                // set item title
+                deleteItem.setTitle("Delete");
+                // set item title fontsize
+                deleteItem.setTitleSize(15);
+                deleteItem.setIcon(R.drawable.ic_delete_sweep_white_24dp);
+                // set item title font color
+                deleteItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        // set creator
+        listView.setMenuCreator(creator);
+        listView.setAdapter(allEventsListAdapter);
+    }
+
+    private ArrayList<EntertainmentModel> getEntertainData() {
+        ArrayList<EntertainmentModel> entertainmentModels = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            EntertainmentModel entertainmentModel = new EntertainmentModel();
+            entertainmentModel.setId(R.drawable.video_hint);
+            entertainmentModel.setTitle("Rarandoi");
+            entertainmentModels.add(entertainmentModel);
+        }
+        return entertainmentModels;
+    }
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getResources().getDisplayMetrics());
     }
 
 }
