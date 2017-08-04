4a3143c69d5ea3f1a41b609e96f11427892a6164
Shankar Pilli <shankar.pilli@sparity.com>
2017-08-04 14:55:51 +0530

    Fixed home screen slow loading issue

--- app/src/main/java/com/xappie/fragments/HomeFragment.java
+++ app/src/main/java/com/xappie/fragments/HomeFragment.java
@@ -195,13 +195,8 @@ public class HomeFragment extends Fragment {
         }
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, rootView);
-        return rootView;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
         initUI();
+        return rootView;
     }
 
     private void initUI() {
