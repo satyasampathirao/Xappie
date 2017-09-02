e1a76df7a17d03f08537c6123dcf00a3278849ca
shankar <shankar.pilli@sparity.com>
2017-09-02 10:23:35 +0530

    Added home content

--- app/src/main/java/com/xappie/utils/APIConstants.java
+++ app/src/main/java/com/xappie/utils/APIConstants.java
@@ -24,5 +24,6 @@ public class APIConstants {
     public static String GET_STORIES = BASE_URL + "get_stories";
     public static String GET_ENTERTAINMENTS = BASE_URL + "get_entertainments";
     public static String GET_NEWS_DETAILS = BASE_URL + "get_news_details/";
+    public static String GET_HOME_CONTENT = BASE_URL + "get_home_content";
 
 }
