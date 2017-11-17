af89a4eb10aa379c205c8f78f1783a130424afb6
Shankar Pilli <shankar.pilli@sparity.com>
2017-11-17 16:49:24 +0530

    Added all the images in the layout

--- app/src/main/java/com/xappie/activities/SplashActivity.java
+++ app/src/main/java/com/xappie/activities/SplashActivity.java
@@ -2,23 +2,36 @@ package com.xappie.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.xappie.R;
 import com.xappie.utils.Constants;
 import com.xappie.utils.Utility;
 
 
 public class SplashActivity extends BaseActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!Utility.isValueNullOrEmpty(Utility.getSharedPrefStringData(this, Constants.SELECTED_COUNTRY_NAME))) {
-            Intent intent = new Intent(this, DashBoardActivity.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(this, AppTourActivity.class);
-            startActivity(intent);
-        }
-        finish();
+        setTheme(R.style.AppTheme_NoActionBar);
+        setContentView(R.layout.splash_activity);
+
+        Handler mSplashHandler = new Handler();
+        Runnable action = new Runnable() {
+            @Override
+            public void run() {
+                if (!Utility.isValueNullOrEmpty(Utility.getSharedPrefStringData(SplashActivity.this, Constants.SELECTED_COUNTRY_NAME))) {
+                    Intent intent = new Intent(SplashActivity.this, DashBoardActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, AppTourActivity.class);
+                    startActivity(intent);
+                }
+                finish();
+            }
+        };
+        mSplashHandler.postDelayed(action, Constants.SPLASH_TIME_OUT);
     }
 }
